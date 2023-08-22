8305824520248d16a662841b7470f62b26570f4c
guilherme2506 <guilherme.augd@gmail.com>
2023-08-22 09:19:56 -0300

    imported AlertProvider and added a main tag

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,10 +1,15 @@
-import {ChakraProvider} from "@chakra-ui/react"
+import {ChakraProvider} from "@chakra-ui/react";
+import { AlertProvider } from "./context/alertContext";
 import './App.css';
 
 function App() {
   return (
   <ChakraProvider>
-    
+    <AlertProvider>
+      <main>
+        
+      </main>
+    </AlertProvider>
   </ChakraProvider>
   );
 }
