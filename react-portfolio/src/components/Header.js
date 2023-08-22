0bc3194df86f7d2038c9bdc696df6b1a8c60ee4f
guilherme2506 <guilherme.augd@gmail.com>
2023-08-22 17:11:35 -0300

    added ChakraUI styling to the Box component

--- react-portfolio/src/components/Header.js
+++ react-portfolio/src/components/Header.js
@@ -21,14 +21,47 @@ const socials = [
 
 const Header = () => {
     const handleClick = (anchor) => () => {
-        const id = `${anchor}-section`;
-        const element = document.getElementById(id);
-
-        if(element) {
-            element.scrollIntoView({
-                behavior: "smooth", 
-                block: "start",
-            });
-        }
-    }
-};
+      const id = `${anchor}-section`;
+      const element = document.getElementById(id);
+      if (element) {
+        element.scrollIntoView({
+          behavior: "smooth",
+          block: "start",
+        });
+      }
+    };
+  
+    return (
+      <Box
+        position="fixed"
+        top={0}
+        left={0}
+        right={0}
+        translateY={0}
+        transitionProperty="transform"
+        transitionDuration=".3s"
+        transitionTimingFunction="ease-in-out"
+        backgroundColor="#18181b"
+      >
+        <Box color="white" maxWidth="1280px" margin="0 auto">
+          <HStack
+            px={16}
+            py={4}
+            justifyContent="space-between"
+            alignItems="center"
+          >
+            <nav>
+              {/* Add social media links based on the `socials` data */}
+            </nav>
+            <nav>
+              <HStack spacing={8}>
+                {/* Add links to Projects and Contact me section */}
+              </HStack>
+            </nav>
+          </HStack>
+        </Box>
+      </Box>
+    );
+  };
+  export default Header;
+  
