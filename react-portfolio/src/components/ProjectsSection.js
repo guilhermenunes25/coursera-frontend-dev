36ca334a76f9fedaedcd935f44e9d161d0381e7f
guilherme2506 <guilherme.augd@gmail.com>
2023-08-24 17:44:04 -0300

    added style for Box

--- react-portfolio/src/components/ProjectsSection.js
+++ react-portfolio/src/components/ProjectsSection.js
@@ -28,8 +28,32 @@ const projects = [
 
 const ProjectsSection = () => {
     return (
-        <FullScreenSection>
-            
+        <FullScreenSection
+        backgroundColor="#14532d"
+        isDarkBackground
+        p={8}
+        alignItems="flex-start"
+        spacing={8}
+        >
+            <Heading as="h1" id="projects-section">
+                Featured Projects
+            </Heading>
+            <Box
+            display="grid"
+            gridTemplateColumns="repeat(2,minmax(0,1fr))"
+            gridGap={8}
+            >
+                {projects.map((project) => (
+                <Card 
+                key={project.title}
+                title={project.title}
+                description={project.description}
+                imageSrc={project.getImageSrc()}
+                />
+                ))}
+            </Box>
         </FullScreenSection>
-    )
-}
+    );
+};
+
+export default ProjectsSection;
