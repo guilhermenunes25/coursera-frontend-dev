8b1823cff8d3bea7b0ff54b6802f0ff68a3eb546
guilherme2506 <guilherme.augd@gmail.com>
2023-08-24 14:44:18 -0300

    added const greeting, bio1 & bio2

---
+++ react-portfolio/src/components/LandingSection.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { Avatar, Heading, Vstack } from "@chakra-ui/react";
+import FullScreenSection from "./FullScreenSection";
+
+const greeting = "Hello, I am Guilherme";
+const bio1 = "A frontend developer";
+const bio2 = "specialised in React";
