8dc264d462a39ddae7f725156ec95d1c81130f35
guilherme2506 <guilherme.augd@gmail.com>
2023-08-24 15:00:27 -0300

    modified text inside const greeting

--- react-portfolio/src/components/LandingSection.js
+++ react-portfolio/src/components/LandingSection.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Avatar, Heading, Vstack } from "@chakra-ui/react";
 import FullScreenSection from "./FullScreenSection";
 
-const greeting = "Hello, I am Guilherme";
+const greeting = "Hello, I am Pete";
 const bio1 = "A frontend developer";
 const bio2 = "specialised in React";
 
