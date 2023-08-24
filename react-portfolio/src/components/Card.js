5d1587aca9976528e97911fa62339167ea2e3c99
guilherme2506 <guilherme.augd@gmail.com>
2023-08-24 17:54:23 -0300

    implemented Heading and HStack

--- react-portfolio/src/components/Card.js
+++ react-portfolio/src/components/Card.js
@@ -6,6 +6,23 @@ import React from "react";
 
 
 const Card = ({title, description, imageSrc}) => {
+    return (
+        <Heading as='h3' size='lg'>
+        Featured Projects
+         <HStack spacing={8}>
+         <Feature
+           title='Plan Money'
+           desc='The future can be even brighter but a goal without a plan is just a wish'
+         />
+         <Feature
+           title='Save Money'
+           desc='You deserve good things. With a whooping 10-15% interest rate per annum, grow your savings on your own terms with our completely automated process'
+         />
+       </HStack>
+        </Heading>    
+    )
+    
+
   // Implement the UI for the Card component according to the instructions.
   // You should be able to implement the component with the elements imported above.
   // Feel free to import other UI components from Chakra UI if you wish to.
