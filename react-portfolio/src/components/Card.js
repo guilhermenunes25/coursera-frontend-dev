6bd55ef92c2608b60c5c8964bafd4e6a105eaafb
guilherme2506 <guilherme.augd@gmail.com>
2023-08-25 11:39:08 -0300

    implemented Chakra UI style to the Card component

--- react-portfolio/src/components/Card.js
+++ react-portfolio/src/components/Card.js
@@ -1,32 +1,35 @@
 import { Heading, HStack, Image, Text, VStack } from "@chakra-ui/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import ProjectsSection from "./ProjectsSection";
 import React from "react";
 
 
 
-const Card = ({title, description, imageSrc}) => {
-    return (
-        <Heading as='h3' size='lg'>
-        Featured Projects
-         <HStack spacing={8}>
-         <Feature
-           title='Plan Money'
-           desc='The future can be even brighter but a goal without a plan is just a wish'
-         />
-         <Feature
-           title='Save Money'
-           desc='You deserve good things. With a whooping 10-15% interest rate per annum, grow your savings on your own terms with our completely automated process'
-         />
-       </HStack>
-        </Heading>    
-    )
+const Card = ({ title, description, imageSrc }) => {
+  return (
+    <Box>
+      <Image
+        boxSize="200px"
+        objectFit="cover"
+        src={imageSrc}
+        alt={title}
+      />
+      <VStack align="start" spacing={2}>
+        <Heading as="h3">
+        {title}
+        </Heading>
+      <Text fontSize="xs">{description}</Text>
+      </VStack>
+      {/* You can add the FontAwesomeIcon and other components as needed */}
+    </Box>
+  );
+};
     
 
   // Implement the UI for the Card component according to the instructions.
   // You should be able to implement the component with the elements imported above.
   // Feel free to import other UI components from Chakra UI if you wish to.
-    return null; 
-};
+    
 
 export default Card;
