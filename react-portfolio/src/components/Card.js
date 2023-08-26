d44fa3891c4f388e396fa014d749d344eefa17bd
guilherme2506 <guilherme.augd@gmail.com>
2023-08-26 18:37:26 -0300

    adjusted the Box and Images size

--- react-portfolio/src/components/Card.js
+++ react-portfolio/src/components/Card.js
@@ -7,18 +7,18 @@ import React from "react";
 
 const Card = ({ title, description, imageSrc }) => {
   return (
-    <Box>
+    <Box p={0} borderWidth="1px" borderRadius="md" boxShadow="md" bg="white">
       <Image
-        boxSize="200px"
-        objectFit="cover"
+        w="100%"
+        h="auto"
         src={imageSrc}
         alt={title}
       />
       <VStack align="start" spacing={2}>
-        <Heading as="h3" size="md">
+        <Heading as="h4" size="md" color={"black"}>
         {title}
         </Heading>
-      <Text fontSize="sm">{description}</Text>
+      <Text fontSize="sm" color={"black"}>{description}</Text>
       </VStack>
       <HStack spacing={2} mt={4}>
       <FontAwesomeIcon icon={faArrowRight} size="1x" />
@@ -28,9 +28,5 @@ const Card = ({ title, description, imageSrc }) => {
 };
     
 
-  // Implement the UI for the Card component according to the instructions.
-  // You should be able to implement the component with the elements imported above.
-  // Feel free to import other UI components from Chakra UI if you wish to.
-    
 
 export default Card;
