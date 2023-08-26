b87a1077cb67f382c722cb9349d5268460c5078d
guilherme2506 <guilherme.augd@gmail.com>
2023-08-26 10:56:17 -0300

    implemented getFieldProps to FormLabel 'Email Address'

--- react-portfolio/src/components/ContactMeSection.js
+++ react-portfolio/src/components/ContactMeSection.js
@@ -69,14 +69,15 @@ const LandingSection = () => {
                     />
                     <FormErrorMessage>{formik.errors.firstName}</FormErrorMessage>
                   </FormControl>
-                  <FormControl isInvalid={false}>
+                  <FormControl isInvalid={formik.touched.email && formik.errors.email}>
                     <FormLabel htmlFor="email">Email Address</FormLabel>
                     <Input
                       id="email"
                       name="email"
                       type="email"
+                      {...formik.getFieldProps("email")}
                     />
-                    <FormErrorMessage></FormErrorMessage>
+                    <FormErrorMessage>{formik.errors.email}</FormErrorMessage>
                   </FormControl>
                   <FormControl>
                     <FormLabel htmlFor="type">Type of enquiry</FormLabel>
