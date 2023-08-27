94e718590a9258b53110a1eb84540c6c5e1fc585
guilherme2506 <guilherme.augd@gmail.com>
2023-08-27 18:07:20 -0300

    added styling to the Heading and Text component

--- react-portfolio/src/components/Card.js
+++ react-portfolio/src/components/Card.js
@@ -17,10 +17,10 @@ const Card = ({ title, description, imageSrc }) => {
         mb={4}
       />
       <VStack align="start" spacing={2}>
-        <Heading as="h4" size="md" color={"black"}>
+        <Heading as="h4" size="md" color={"#36454F"} fontSize="1x" ml={4}>
         {title}
         </Heading>
-      <Text fontSize="sm" color={"black"}>{description}</Text>
+      <Text fontSize="sm" color={"#36454F"} mt={4} ml={4}>{description}</Text>
       </VStack>
       <HStack spacing={2} mt={4}>
       <FontAwesomeIcon icon={faArrowRight} size="1x" />
