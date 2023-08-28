2d1610df8e77e761ce4d1dc9309c1549e5329a79
guilherme2506 <guilherme.augd@gmail.com>
2023-08-28 14:41:16 -0300

    fix export statement

--- react-portfolio/src/components/LandingSection.js
+++ react-portfolio/src/components/LandingSection.js
@@ -1,38 +1,31 @@
 import React from "react";
-import { Avatar, Heading, VStack, Text, StackDivider } from "@chakra-ui/react";
+import { Avatar, Heading, VStack, Text } from "@chakra-ui/react";
 import FullScreenSection from "./FullScreenSection";
 
 const greeting = "Hello, I am Pete!";
 const bio1 = "A frontend developer";
 const bio2 = "specialised in React";
 
-// Implement the UI for the LandingSection component according to the instructions.
-// Use a combination of Avatar, Heading and VStack components.
-
 const LandingSection = () => (
-    <FullScreenSection
-      justifyContent="center"
-      alignItems="center"
-      isDarkBackground
-      backgroundColor="#2A4365"
-    >
-        
+  <FullScreenSection
+    justifyContent="center"
+    alignItems="center"
+    isDarkBackground
+    backgroundColor="#2A4365"
+  >
     <Avatar src="https://i.pravatar.cc/150?img=7" name="Pete" size="150px" />
-    <Heading as='h5' size='sm'>
-    {greeting}
-  </Heading>
-    <VStack
-    spacing={0.1}
-    align='center'
-    >
-      <Text fontSize="48px" fontWeight={"bold"} color="white">
+    <Heading as="h5" size="sm">
+      {greeting}
+    </Heading>
+    <VStack spacing={0.1} align="center">
+      <Text fontSize="48px" fontWeight="bold" color="white">
         {bio1}
       </Text>
-      <Text fontSize="48px" fontWeight={"bold"} color="white">
+      <Text fontSize="48px" fontWeight="bold" color="white">
         {bio2}
       </Text>
     </VStack>
-    </FullScreenSection>
+  </FullScreenSection>
 );
 
 export default LandingSection;
