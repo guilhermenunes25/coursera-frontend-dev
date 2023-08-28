946ea893c20cb8a30c7552760150f8145bf30458
guilherme2506 <guilherme.augd@gmail.com>
2023-08-28 10:24:15 -0300

    implemented useEffect, useState and useRef hooks

--- react-portfolio/src/components/Header.js
+++ react-portfolio/src/components/Header.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import {Box,HStack} from "@chakra-ui/react";
@@ -33,29 +33,55 @@ const socials = [
 ];
 
 const Header = () => {
-    const handleClick = (anchor) => () => {
-      const id = `${anchor}-section`;
-      const element = document.getElementById(id);
-      if (element) {
-        element.scrollIntoView({
-          behavior: "smooth",
-          block: "start",
-        });
-      }
+  const [prevScrollY, setPrevScrollY] = useState(0);
+  const boxRef = useRef(null);
+
+  const handleScroll = () => {
+    const currentScrollY = window.scrollY;
+
+    if (currentScrollY > prevScrollY) {
+      // Scrolling down
+      boxRef.current.style.transform = "translateY(-200px)";
+    } else {
+      // Scrolling up
+      boxRef.current.style.transform = "translateY(0)";
+    }
+
+    setPrevScrollY(currentScrollY);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
     };
+  }, []);
+
+  const handleClick = (anchor) => () => {
+    const id = `${anchor}-section`;
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
+    }
+  };
   
     return (
       <Box
-        position="fixed"
-        top={0}
-        left={0}
-        right={0}
-        translateY={0}
-        transitionProperty="transform"
-        transitionDuration=".3s"
-        transitionTimingFunction="ease-in-out"
-        backgroundColor="#18181b"
-        zIndex={1000}
+      ref={boxRef}
+      position="fixed"
+      top={0}
+      left={0}
+      right={0}
+      translateY={0}
+      transitionProperty="transform"
+      transitionDuration=".3s"
+      transitionTimingFunction="ease-in-out"
+      backgroundColor="#18181b"
+      zIndex={1000}
       >
         <Box color="white" maxWidth="1280px" margin="0 auto">
           <HStack px={16} py={4} justifyContent="space-between" alignItems="center">
