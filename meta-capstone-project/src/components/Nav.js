345cce52abbb31a97d663b65f4a2ab3b86734e73
guilherme2506 <guilherme.augd@gmail.com>
2023-10-04 10:20:35 -0300

    added a unordered list w/ links

--- meta-capstone-project/src/components/Nav.js
+++ meta-capstone-project/src/components/Nav.js
@@ -5,12 +5,12 @@ export default function Nav () {
         <>
         <img src="../assets/Logo.svg" alt="Little Lemon Logo"/>
         <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Menu</li>
-            <li>Reservations</li>
-            <li>Order Online</li>
-            <li>Login</li>
+            <li><a href="/">Home</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/menu">Menu</a></li>
+            <li><a href="/reservations">Reservations</a></li>
+            <li><a href="/order-online">Order Online</a></li>
+            <li><a href="/login">Login</a></li>
         </ul>
         </>
     )
