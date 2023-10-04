66f28b69b6096c2633e0e4043e53761a4f06678e
guilherme2506 <guilherme.augd@gmail.com>
2023-10-04 10:11:24 -0300

    fix export the export function statement

--- meta-capstone-project/src/components/Nav.js
+++ meta-capstone-project/src/components/Nav.js
@@ -1,3 +1,17 @@
+import React from 'react';
+
 export default function Nav () {
-    
+    return (
+        <>
+        <img />
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Menu</li>
+            <li>Reservations</li>
+            <li>Order Online</li>
+            <li>Login</li>
+        </ul>
+        </>
+    )
 }
