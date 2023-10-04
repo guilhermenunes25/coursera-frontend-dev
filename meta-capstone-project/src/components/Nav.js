23b25e604d4f02a61cecd0b0b835bd2d39010291
guilherme2506 <guilherme.augd@gmail.com>
2023-10-04 10:32:39 -0300

    imported the Logo.svg image

--- meta-capstone-project/src/components/Nav.js
+++ meta-capstone-project/src/components/Nav.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Logo from '../assets/Logo.svg';
 
 export default function Nav () {
     return (
         <>
-        <img src="../assets/Logo.svg" alt="Little Lemon Logo"/>
+        <img src={require("../assets/Logo.svg").default} alt="Little Lemon Logo"/>
         <ul>
             <li><a href="/">Home</a></li>
             <li><a href="/about">About</a></li>
@@ -15,3 +16,4 @@ export default function Nav () {
         </>
     )
 }
+
