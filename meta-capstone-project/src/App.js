8229e0e80a944ebe6f6f1b118426165582e2781d
guilherme2506 <guilherme.augd@gmail.com>
2023-10-04 09:35:49 -0300

    imported the Footer component

--- meta-capstone-project/src/App.js
+++ meta-capstone-project/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Nav from './components/Nav';
 import Header from './components/Header';
 import Main from './components/Main';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
     <Nav />
     <Header />
     <Main />
+    <Footer />
     </>
   );
 }
