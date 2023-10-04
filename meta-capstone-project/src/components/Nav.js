388651cb057258cd5cdac47758f0256dc15aaa86
guilherme2506 <guilherme.augd@gmail.com>
2023-10-04 11:18:16 -0300

    updated nav tag

--- meta-capstone-project/src/components/Nav.js
+++ meta-capstone-project/src/components/Nav.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Logo from '../assets/Logo.svg';
+import './Nav.css' 
 
 export default function Nav () {
     const imageStyles = {
@@ -8,9 +9,9 @@ export default function Nav () {
     }
 
     return (
-        <>
-        <img src={require("../assets/Logo.svg").default} alt="Little Lemon Logo" style={imageStyles}/>
-        <ul>
+        <nav>
+        <img src={require("../assets/Logo.svg").default} alt="Little Lemon Logo" className="logo"/>
+        <ul className="nav-menu">
             <li><a href="/">Home</a></li>
             <li><a href="/about">About</a></li>
             <li><a href="/menu">Menu</a></li>
@@ -18,7 +19,7 @@ export default function Nav () {
             <li><a href="/order-online">Order Online</a></li>
             <li><a href="/login">Login</a></li>
         </ul>
-        </>
+        </nav>
     )
 }
 
