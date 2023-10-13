95a5b4286ad64f87e8f152260870bc021dbe27bb
guilherme2506 <guilherme.augd@gmail.com>
2023-10-13 15:41:35 -0300

    added Online Menu button tag

--- meta-capstone-project/src/components/SpecialOffers.js
+++ meta-capstone-project/src/components/SpecialOffers.js
@@ -7,13 +7,15 @@ export default function EspecialOffers () {
         <>
             <div div className="container">
                 <h2 className="main-header">This Weeks Specials!</h2>
+                <button className="online-menu-button">Online Menu</button>
+                <div className="cards-container">
                 <div className="card" id="card1">
                     <img src="assets/menu.jpg" alt="menu-card" />
                     <h2 className="card-title">Greek Salad</h2>
                     <p>
                         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error, eos fugiat nisi sequi sit quaerat! Ipsa rem commodi illo adipisci nisi. Reiciendis nihil magnam reprehenderit similique esse fugiat, accusamus autem hic quod ab et adipisci numquam ex doloribus cumque inventore quibusdam ipsum, commodi saepe. Harum.
                     </p>
-                    <button> Read more</button>
+                    <a href="#" className="card-button">Order a delivery</a>
                 </div>
                 <div className="card" id="card2">
                     <img src="assets/book.jpg" alt="book-table"/>
@@ -21,7 +23,7 @@ export default function EspecialOffers () {
                     <p>
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti ex pariatur repellendus hic accusamus! Esse deserunt maxime facere sapiente minus officia ullam, vitae iure dignissimos fugit est quisquam laudantium voluptatibus neque aliquid, laboriosam voluptatem ipsa. Ab a explicabo quaerat reiciendis totam ut iure officiis nisi?
                     </p>
-                    <button> Read more</button>
+                    <a href="#" className="card-button">Order a delivery</a>
                 </div>
                 <div className="card" id="card3">
                     <img src="assets/about.jpg" alt="about-info"/>
@@ -29,7 +31,8 @@ export default function EspecialOffers () {
                     <p>
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, voluptatibus consequatur non unde commodi officia a! Eligendi rem, consequatur nam, esse ipsam quasi facilis suscipit illum quae id odio dolorem itaque nemo? Blanditiis, voluptatum? Iure, blanditiis quo. Perferendis maiores soluta illum molestias ut rerum quae?
                     </p>
-                    <button> Read more</button>
+                    <a href="#" className="card-button">Order a delivery</a>
+                </div>
                 </div>
                 </div>
         </>
