1bfd790b8e58f6c0dd13af3b4703aaf0f14f07c2
guilherme2506 <guilherme.augd@gmail.com>
2023-10-14 10:25:16 -0300

    updated the restaurant-description

--- meta-capstone-project/src/components/AppHeader.js
+++ meta-capstone-project/src/components/AppHeader.js
@@ -10,8 +10,8 @@ return (
             <div className="header-left">
                 <h1 className="restaurant-title">Little Lemon</h1>
                 <p className="restaurant-subtitle">Chicago</p>
-                <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                <p className="restaurant-description">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
                 </p>
                    <button className="reserve-a-table-button">Reserve a Table</button>
             </div>
