fe3f94efaf361b9d21d213ccc7aa539c5e990ab8
guilherme2506 <guilherme.augd@gmail.com>
2023-10-16 11:15:39 -0300

    imported image files

--- meta-capstone-project/src/components/SpecialOffers.js
+++ meta-capstone-project/src/components/SpecialOffers.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import '../styles/SpecialOffersStyle.css';
+import greekSalad from '../assets/greek-salad.jpg';
+import bruschettaImg from '../assets/bruschetta.jpg';
+import lemonDessert from '../assets/lemon-dessert.jpg';
 
 
 export default function EspecialOffers () {
@@ -12,7 +15,7 @@ export default function EspecialOffers () {
                 </div>
                 <div className="cards-container">
                 <div className="card" id="card1">
-                    <img src="assets/menu.jpg" alt="menu-card" />
+                    <img src={greekSalad} alt="greek-salad" />
                     <h2 className="card-title">Greek Salad</h2>
                     <p>
                         The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons.
@@ -20,7 +23,7 @@ export default function EspecialOffers () {
                     <a href="#" className="card-button">Order a delivery</a>
                 </div>
                 <div className="card" id="card2">
-                    <img src="assets/book.jpg" alt="book-table"/>
+                    <img src={bruschettaImg} alt="bruschetta"/>
                     <h2 className="card-title">Bruschetta</h2>
                     <p>
                         Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil.
@@ -28,7 +31,7 @@ export default function EspecialOffers () {
                     <a href="#" className="card-button">Order a delivery</a>
                 </div>
                 <div className="card" id="card3">
-                    <img src="assets/about.jpg" alt="about-info"/>
+                    <img src={lemonDessert} alt="lemon-dessert"/>
                     <h2 className="card-title">Lemon Dessert</h2>
                     <p>
                         This comes straight from grandma's recipe book, every last ingredient has been sourced and is as authentic as can be imagined. 
