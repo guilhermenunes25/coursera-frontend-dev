1cb5b11a43c6432395bf9549d67e79d5a3385d46
guilherme2506 <guilherme.augd@gmail.com>
2023-10-17 09:35:04 -0300

    imported fontawesome icon to the component

--- meta-capstone-project/src/components/SpecialOffers.js
+++ meta-capstone-project/src/components/SpecialOffers.js
@@ -3,6 +3,8 @@ import '../styles/SpecialOffersStyle.css';
 import greekSalad from '../assets/greek-salad.jpg';
 import bruschettaImg from '../assets/bruschetta.jpg';
 import lemonDessert from '../assets/lemon-dessert.jpg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBasketShopping } from '@fortawesome/free-solid-svg-icons';
 
 
 
@@ -24,7 +26,7 @@ export default function EspecialOffers () {
                          <p className="card-description">
                         The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons.
                          </p>
-                         <img src={basketIcon} alt="Basket Icon" className="basket-icon" />
+                         <FontAwesomeIcon icon={faBasketShopping} style={{ color: '#333333' }} />
                                 <a href="#" className="card-button">Order a delivery</a>
                 </div>
                 <div className="card" id="card2">
@@ -36,7 +38,7 @@ export default function EspecialOffers () {
                          <p className="card-description">
                         Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil.
                          </p>
-                         <img src={basketIcon} alt="Basket Icon" className="basket-icon" />
+                         <FontAwesomeIcon icon={faBasketShopping} style={{ color: '#333333' }} />
                                 <a href="#" className="card-button">Order a delivery</a>
                 </div>
                 <div className="card" id="card3">
@@ -48,7 +50,7 @@ export default function EspecialOffers () {
                         <p className="card-description">
                         This comes straight from grandma's recipe book, every last ingredient has been sourced and is as authentic as can be imagined. 
                         </p>
-                        <img src={basketIcon} alt="Basket Icon" className="basket-icon" />
+                        <FontAwesomeIcon icon={faBasketShopping} style={{ color: '#333333' }} />
                                 <a href="#" className="card-button">Order a delivery</a>
                         </div>
                     </div>
