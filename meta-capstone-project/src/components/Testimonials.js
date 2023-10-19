d819deac479a0a0775ec69f9d5c1d582fa9cebfc
guilherme2506 <guilherme.augd@gmail.com>
2023-10-19 15:54:33 -0300

    updated jsx code

--- meta-capstone-project/src/components/Testimonials.js
+++ meta-capstone-project/src/components/Testimonials.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../styles/TestimonialsStyle.css'
 import user1 from '../assets/user1.jpg';
 import user2 from '../assets/user2.jpg';
 import user3 from '../assets/user3.jpg';
@@ -8,25 +9,25 @@ export default function Testimonials () {
 
   const testimonials = [
     {
-      rating: '⭐️⭐️⭐️⭐️⭐️',
+      rating: 'Rating: ⭐️⭐️⭐️⭐️⭐️',
       photo: user1,
       name: 'John Doe',
-      review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      review: 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
     },
     {
-      rating:'⭐️⭐️⭐️⭐️⭐️',
+      rating:'Rating: ⭐️⭐️⭐️⭐️⭐️',
       photo: user2,
       name: 'Jane Smith',
       review: 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
     },
     {
-      rating:'⭐️⭐️⭐️⭐️⭐️',
+      rating:'Rating: ⭐️⭐️⭐️⭐️⭐️',
       photo: user3,
       name: 'Aaron Durant',
       review: 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
     },
     {
-      rating:'⭐️⭐️⭐️⭐️⭐️',
+      rating:'Rating: ⭐️⭐️⭐️⭐️⭐️',
       photo: user4,
       name: 'Rebecca Maya',
       review: 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
@@ -34,19 +35,19 @@ export default function Testimonials () {
 
     
   ];
-    return (
-      <div>
-        <h2 className="testimonials-header">Testimonials</h2>
-        <div className="testimonial-card-container">
-          {testimonials.map((testimonial, index) => ( 
-            <div className="testimonial-card" key={index}>
-              <div className="rating">{testimonial.rating}</div>
-              <img className="reviewer-photo" src={testimonial.photo} alt={testimonial.name} />
-              <div className="reviewer-name">{testimonial.name}</div>
-              <p className="review-text">{testimonial.review}</p>
-            </div>
-          ))}
-        </div>
-      </div>  
-    )
+  return (
+    <div className="testimonials-container">
+      <h2 className="testimonials-header">Testimonials</h2>
+      <div className="testimonials-card-container">
+        {testimonials.map((testimonial, index) => (
+          <div className="testimonials-card" key={index}>
+            <div className="rating">{testimonial.rating}</div>
+            <img className="reviewer-photo" src={testimonial.photo} alt={testimonial.name} />
+            <div className="reviewer-name">{testimonial.name}</div>
+            <p className="review-text">{testimonial.review}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
