dacaf8407db2fe7c3dc3902eb3410d8ca44d0acb
guilherme2506 <guilherme.augd@gmail.com>
2023-10-23 15:33:15 -0300

    update text description

--- meta-capstone-project/src/components/AppAbout.js
+++ meta-capstone-project/src/components/AppAbout.js
@@ -5,30 +5,30 @@ import '../styles/AboutStyles.css'
 
 export default function AppAbout () {
     return (
+        <div className="about-main">
         <div className="about-content">
         <div className="about-left">
             <h1 className="about-title">Little Lemon</h1>
                 <p className="about-subtitle">Chicago</p>
                     <p className="about-description">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                     </p>
         </div>
-        <div className="header-right">
-            <div className="about-image-a">
-        <img
-            src={MarioAdrianA}  // Make sure the path is correct
-            alt="About Banner"
-            className="restaurant-banner"
-        />  
-          </div>
-          <div className="about-image-b">
-        <img
-            src={MarioAdrianB}  // Make sure the path is correct
-            alt="Restaurant Banner"
-            className="restaurant-banner"
-        />
-          </div>
+        <div className="about-right">
+                    <div className="about-image-container">
+                        <img
+                            src={MarioAdrianA}
+                            alt="Image A"
+                            className="about-image-a"
+                        />
+                        <img
+                            src={MarioAdrianB}
+                            alt="Image B"
+                            className="about-image-b"
+                        />
+                    </div>
+                </div>
+            </div>
         </div>
-    </div>
     )
 }
