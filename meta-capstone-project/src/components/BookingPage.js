6b590034aa4ffbe5f65de1556df5f2aa59109b37
guilherme2506 <guilherme.augd@gmail.com>
2023-10-31 16:06:11 -0300

    moved file to the right directory

---
+++ meta-capstone-project/src/components/BookingPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import BookingForm from './BookingForm';
+
+export default function BookingPage() {
+    return (
+        <div>
+            <h1>Reservations</h1>
+            <p>Book a table at Little Lemon</p>
+            <BookingForm />
+        </div>
+    )
+}
