f0e52269a41e18d487c69e3924288e86b40bf98f
guilherme2506 <guilherme.augd@gmail.com>
2023-10-31 16:12:58 -0300

    imported react-router-dom and BookingPage component

--- meta-capstone-project/src/App.js
+++ meta-capstone-project/src/App.js
@@ -5,11 +5,17 @@ import SpecialOffers from './components/SpecialOffers';
 import AppFooter from './components/AppFooter';
 import Testimonials from './components/Testimonials';
 import AppAbout from './components/AppAbout';
+import BookingPage from './components/BookingPage'
+
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <>  
     <AppNav />
+    <Switch>
+      <Route path="/reservations" component={BookingPage} />
+    </Switch>
     <AppHeader />
     <SpecialOffers />
     <Testimonials />
