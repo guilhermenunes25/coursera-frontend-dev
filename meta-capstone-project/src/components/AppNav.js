c26ce122369f8c15d07ea8cea12a3b4ba76e4383
guilherme2506 <guilherme.augd@gmail.com>
2023-10-31 16:09:27 -0300

    implemented the react-router-dom to the list

--- meta-capstone-project/src/components/AppNav.js
+++ meta-capstone-project/src/components/AppNav.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import '../styles/NavStyle.css' 
+import '../styles/NavStyle.css' ;
+import {Link} from 'react-router-dom';
 
 export default function AppNav () {
     return (
         <nav className='navbar'>
         <img src={require("../assets/Logo.svg").default} alt="Little Lemon Logo" className="logo"/>
         <ul className="nav-menu">
-            <li><a href="/">Home</a></li>
-            <li><a href="/about">About</a></li>
-            <li><a href="/menu">Menu</a></li>
-            <li><a href="/reservations">Reservations</a></li>
-            <li><a href="/order-online">Order Online</a></li>
-            <li><a href="/login">Login</a></li>
-        </ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/menu">Menu</Link></li>
+        <li><Link to="/reservations">Reservations</Link></li> {/* Link to the BookingPage */}
+        <li><Link to="/order-online">Order Online</Link></li>
+        <li><Link to="/login">Login</Link></li>
+      </ul>
         </nav>
     )
 }
