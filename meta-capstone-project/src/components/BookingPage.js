59fb6c5da1c6fdc4435e10b6c27f40556e04974a
guilherme2506 <guilherme.augd@gmail.com>
2023-11-02 12:18:13 -0300

    moved useState hook to the component

--- meta-capstone-project/src/components/BookingPage.js
+++ meta-capstone-project/src/components/BookingPage.js
@@ -2,11 +2,37 @@ import React from 'react';
 import BookingForm from './BookingForm';
 
 export default function BookingPage() {
+    const [formData, setFormData] = useState({
+        date: '',
+        time: '17:00',
+        guests: 1,
+        occasion: 'Birthday'
+    });
+
+    const availableTimes = [
+        '17:00',
+        '18:00',
+        '19:00',
+        '20:00',
+        '21:00',
+        '22:00',
+    ];
+
+    const handleInputChange = (name, value) => {
+        setFormData({
+            ...formData,
+            [name]: value,
+        });
+    };
+
     return (
         <div>
-            <h1>Reservations</h1>
-            <p>Book a table at Little Lemon</p>
-            <BookingForm />
+            <h1>Booking Page</h1>
+            <BookingForm 
+            formData= {formData}
+            availableTimes={availableTimes}
+            onInputChange={handleInputChange}
+            />
         </div>
     )
 }
