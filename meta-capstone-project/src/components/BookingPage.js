1c11b175957c07b4b4d7269e18b0612f29e902b6
guilherme2506 <guilherme.augd@gmail.com>
2023-11-03 22:28:40 -0300

    implemented useReducer hook

--- meta-capstone-project/src/components/BookingPage.js
+++ meta-capstone-project/src/components/BookingPage.js
@@ -1,38 +1,54 @@
-import React from 'react';
+import React, { useState, useReducer } from 'react';
 import BookingForm from './BookingForm';
 
+// Define a reducer function
+function availableTimesReducer(state, action) {
+  if (action.type === 'UPDATE_TIMES') {
+    if (action.date === '2023-11-01') {
+      return ['17:00', '18:00', '19:00'];
+    } else if (action.date === '2023-11-02') {
+      return ['19:00', '20:00', '21:00'];
+    } else if (action.date === '2023-11-03') {
+      return ['18:00', '20:00', '22:00'];
+    } else {
+      return ['17:00', '18:00', '19:00', '20:00', '21:00', '22:00'];
+    }
+  } else {
+    return state;
+  }
+}
+
 export default function BookingPage() {
-    const [formData, setFormData] = useState({
-        date: '',
-        time: '17:00',
-        guests: 1,
-        occasion: 'Birthday'
+  const [formData, setFormData] = useState({
+    date: '',
+    time: '17:00',
+    guests: 1,
+    occasion: 'Birthday',
+  });
+
+  const [availableTimes, dispatch] = useReducer(availableTimesReducer, []);
+
+  const handleInputChange = (name, value) => {
+    setFormData({
+      ...formData,
+      [name]: value,
     });
 
-    const availableTimes = [
-        '17:00',
-        '18:00',
-        '19:00',
-        '20:00',
-        '21:00',
-        '22:00',
-    ];
+    if (name === 'date') {
+      // Dispatch an action to update available times based on the selected date
+      dispatch({ type: 'UPDATE_TIMES', date: value });
+    }
+  };
 
-    const handleInputChange = (name, value) => {
-        setFormData({
-            ...formData,
-            [name]: value,
-        });
-    };
-
-    return (
-        <div>
-            <h1>Booking Page</h1>
-            <BookingForm 
-            formData= {formData}
-            availableTimes={availableTimes}
-            onInputChange={handleInputChange}
-            />
-        </div>
-    )
+  return (
+    <div>
+      <h1>Booking Page</h1>
+      <BookingForm
+        formData={formData}
+        availableTimes={availableTimes}
+        onInputChange={handleInputChange}
+        dispatch={dispatch}
+      />
+    </div>
+  );
 }
