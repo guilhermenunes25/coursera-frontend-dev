e932a556bf8154a9244e0fba4aa6401c4b22fa86
guilherme2506 <guilherme.augd@gmail.com>
2023-11-07 11:43:27 -0300

    fixed useState hook implementation

--- meta-capstone-project/src/components/BookingForm.js
+++ meta-capstone-project/src/components/BookingForm.js
@@ -1,53 +1,62 @@
-import React from 'react';
+import React, {useState} from 'react';
 
+export default function BookingForm() {
+//variables for the form fields;
+const [date, setDate] = useState('');
+const [time, setTime] = useState('');
+const [guests, setGuests] = useState('');
+const [occasion, setOccasion] = useState('');
 
-export default function BookingForm( {formData, availableTimes, onInputChange}) {
+//variables for available time options;
+const [availableTimes, setAvailableTimes] = useState([
+    '17:00', '18:00', '19:00', '20:00', '21:00', '22:00'
+]);
 
-    return (
-       <form style={{ display: 'grid', maxWidth: '200px', gap: '20px'}}>
-            <label htmlFor="res-date">Choose date</label>
-            <input 
-                type="date" 
-                id="res-date" 
-                name="date"
-                value={formData.date}
-                onChange={(e) => onInputChange('date', e.target.value)}    
-            />
-            <label>Choose time</label>
-            <select 
-                id="res-time"
-                name="time"
-                value={formData.time}
-                onChange={(e) => onInputChange('time', e.target.value)} 
-            >
-                {availableTimes.map((time) => (
-                    <option key={time} value={time}>
-                        {time}
-                    </option>
-                ))}
-            </select>
-            <label htmlFor="guests">Number of guests</label>
-            <input 
-                type="number" 
-                placeholder="1" 
-                min="1" 
-                max="10" 
-                id="guests"
-                name="guests"
-                value={formData.guests}
-                onChange={(e) => onInputChange('guests', parseInt(e.target.value, 10))} 
-                />
-            <label htmlFor="occasion">Occasion</label>
-            <select 
-                id="occasion"
-                name="occasion"
-                value={formData.occasion}
-                onChange={(e) => onInputChange('occasion', e.target.value)} 
-            >
-                <option value="Birthday">Birthday</option>
-                <option value="Anniversary">Anniversary</option>
-            </select>
-            <input type="submit" value="Make Your reservation"/>
-       </form>
-    )
+return (
+    <form style={{ display: 'grid', maxWidth: '200px', gap: '20px' }}>
+      <label htmlFor="res-date">Choose date</label>
+      <input
+        type="date"
+        id="res-date"
+        value={date}
+        onChange={(e) => setDate(e.target.value)}
+      />
+
+      <label htmlFor="res-time">Choose time</label>
+      <select
+        id="res-time"
+        value={time} 
+        onChange={(e) => setTime(e.target.value)}
+      >
+        <option value="">Select a time</option> 
+        {availableTimes.map((option, index) => (
+          <option key={index} value={option}>
+            {option}
+          </option>
+        ))}
+      </select>
+
+      <label htmlFor="guests">Number of guests</label>
+      <input
+        type="number"
+        placeholder="1"
+        min="1"
+        max="10"
+        id="guests"
+        value={guests}
+        onChange={(e) => setGuests(e.target.value)}
+      />
+
+      <label htmlFor="occasion">Occasion</label>
+      <select
+        id="occasion"
+        value={occasion}
+        onChange={(e) => setOccasion(e.target.value)}
+      >
+        <option value="Birthday">Birthday</option>
+        <option value="Anniversary">Anniversary</option>
+      </select>
+      <input type="submit" value="Make Your reservation" />
+    </form>
+  );
 }
