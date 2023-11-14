71e8f3c4d0900c99c4690b094f24025c5d3bf26a
guilherme2506 <guilherme.augd@gmail.com>
2023-11-14 11:06:42 -0300

    fixed Link path for reservations

--- meta-capstone-project/src/components/AppNav.js
+++ meta-capstone-project/src/components/AppNav.js
@@ -2,19 +2,24 @@ import React from 'react';
 import '../styles/NavStyle.css' ;
 import {Link} from 'react-router-dom';
 
-export default function AppNav () {
-    return (
-        <nav className='navbar'>
-        <img src={require("../assets/Logo.svg").default} alt="Little Lemon Logo" className="logo"/>
-        <ul className="nav-menu">
+export default function AppNav() {
+  const handleReservationsClick = () => {
+    // Open BookingPage in a new window
+    window.open('/reservations', '_blank');
+  };
+
+  return (
+    <nav className='navbar'>
+      <img src={require("../assets/Logo.svg").default} alt="Little Lemon Logo" className="logo" />
+      <ul className="nav-menu">
         <li><Link to="/">Home</Link></li>
         <li><Link to="/about">About</Link></li>
         <li><Link to="/menu">Menu</Link></li>
-        <li><Link to="/reservations">Reservations</Link></li> {/* Link to the BookingPage */}
+        <li onClick={handleReservationsClick}>Reservations</li>
         <li><Link to="/order-online">Order Online</Link></li>
         <li><Link to="/login">Login</Link></li>
       </ul>
-        </nav>
-    )
+    </nav>
+  );
 }
 
