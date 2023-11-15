09ed3afc09c2ce3b764f5c230854990f0b28684a
guilherme2506 <guilherme.augd@gmail.com>
2023-11-15 11:59:34 -0300

    updated Routes component

--- meta-capstone-project/src/App.js
+++ meta-capstone-project/src/App.js
@@ -1,29 +1,36 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AppNav from './components/AppNav';
 import AppHeader from './components/AppHeader';
 import SpecialOffers from './components/SpecialOffers';
 import AppFooter from './components/AppFooter';
 import Testimonials from './components/Testimonials';
 import AppAbout from './components/AppAbout';
-import BookingPage from './components/BookingPage'
-
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import BookingLayout from './components/BookingLayout';
 
 function App() {
   return (
     <Router>
       <div>
-      <AppNav />
-      <Routes>
-        <Route path="/reservations" element={<BookingPage />} />
-      </Routes>
+        <AppNav />
+        <Routes>
+          <Route path="/" element={<MainContent />} />
+          <Route path="/reservations" element={<BookingLayout />} />
+        </Routes>
       </div>
+    </Router>
+  );
+}
+
+function MainContent() {
+  return (
+    <div>
       <AppHeader />
       <SpecialOffers />
       <Testimonials />
       <AppAbout />
       <AppFooter />
-    </Router>
+    </div>
   );
 }
 
