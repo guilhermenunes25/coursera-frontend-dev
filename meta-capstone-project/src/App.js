99a7d6069ab5203b8e8d4fa546a80e43363dd77b
guilherme2506 <guilherme.augd@gmail.com>
2023-11-20 15:43:47 -0300

    implemented Routes for the ConfirmedBooking component

--- meta-capstone-project/src/App.js
+++ meta-capstone-project/src/App.js
@@ -7,6 +7,7 @@ import AppFooter from './components/AppFooter';
 import Testimonials from './components/Testimonials';
 import AppAbout from './components/AppAbout';
 import BookingLayout from './components/BookingLayout';
+import ConfirmedBooking from './components/ConfirmedBooking';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Routes>
           <Route path="/" element={<MainContent />} />
           <Route path="/reservations" element={<BookingLayout />} />
+          <Route path="/reservations/confirmed" element={<ConfirmedBooking />} />
         </Routes>
       </div>
     </Router>
