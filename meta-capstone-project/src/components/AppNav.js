bd71ac47574f0125373c7b9a1d4679f86e0c256e
guilherme2506 <guilherme.augd@gmail.com>
2023-11-27 14:54:34 -0300

    added a class to the reservation list item

--- meta-capstone-project/src/components/AppNav.js
+++ meta-capstone-project/src/components/AppNav.js
@@ -15,7 +15,7 @@ export default function AppNav() {
         <li><Link to="/">Home</Link></li>
         <li><Link to="/about">About</Link></li>
         <li><Link to="/menu">Menu</Link></li>
-        <li onClick={handleReservationsClick}>Reservations</li>
+        <li className="special" onClick={handleReservationsClick}>Reservations</li>
         <li><Link to="/order-online">Order Online</Link></li>
         <li><Link to="/login">Login</Link></li>
       </ul>
