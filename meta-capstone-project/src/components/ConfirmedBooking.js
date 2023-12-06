f222cc633857b31d8dda2799e34fd5d331a2dd35
guilherme2506 <guilherme.augd@gmail.com>
2023-12-06 17:47:43 -0300

    added a return button

--- meta-capstone-project/src/components/ConfirmedBooking.js
+++ meta-capstone-project/src/components/ConfirmedBooking.js
@@ -1,10 +1,13 @@
-import React from "react";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-export default function ConfirmedBooking () {
-     return (
-        <div>
-            <h2> Booking Confirmed!</h2>
-            <p>Your booking has been successfully confirmed. Thank you!</p>
-        </div>
-     );   
+export default function ConfirmedBooking() {
+
+  return (
+    <div>
+      <h2>Booking Confirmed!</h2>
+      <p>Your booking has been successfully confirmed. Thank you!</p>
+      <Link to="/">Return</Link>
+    </div>
+  );
 }
