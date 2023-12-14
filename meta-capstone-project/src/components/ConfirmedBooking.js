f8b8cfa55d45b3aa61c8541b2f92fd579fd4c863
guilherme2506 <guilherme.augd@gmail.com>
2023-12-13 23:20:10 -0300

    updated import statements

--- meta-capstone-project/src/components/ConfirmedBooking.js
+++ meta-capstone-project/src/components/ConfirmedBooking.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import AppFooter from './AppFooter'; 
+import { Link } from 'react-router-dom'; 
 import '../styles/ConfirmedBookingStyling.css';
 export default function ConfirmedBooking() {
 
