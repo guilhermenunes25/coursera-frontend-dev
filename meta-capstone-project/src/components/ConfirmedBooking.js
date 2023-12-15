484914b39db98da514ce47245108776a9e08a863
guilherme2506 <guilherme.augd@gmail.com>
2023-12-15 15:22:49 -0300

    updated the return link text

--- meta-capstone-project/src/components/ConfirmedBooking.js
+++ meta-capstone-project/src/components/ConfirmedBooking.js
@@ -7,7 +7,7 @@ export default function ConfirmedBooking() {
     <div>
       <h2 className='confirmed-header'>Booking Confirmed!</h2>
       <p className='confirmed-description'>Your booking has been successfully confirmed. Thank you!</p>
-      <Link to="/" className='return-button'>Return</Link>
+      <Link to="/" className='return-button'>Home Page</Link>
     </div>
   );
 }
